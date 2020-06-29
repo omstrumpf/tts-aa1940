9e504946ac91dd3d1d5d00feced07fa8289ef832
omstrumpf <omstrumpf@gmail.com>
2020-06-28 22:30:47 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tabletop Simulator - Axis & Allies 1940 Scripting
+
+## Features
+
+- Battle board hit counting
+- Income & bonus objective automation
+- Miniature spawning hotkeys
---
+++ global.lua
@@ -0,0 +1,479 @@
+-- Axis & Allies 1940 Global Scripting
+--
+-- Battle Board: counts hits automatically based on dice zones
+-- Spawning: units, chits, etc. bound to numpad keys. color/power aware.
+-- Income: Buttons to collect income for the turn, including national objectives and bonus income
+
+------ BAG GUIDS
+trashBag = "c0a4e5"
+
+chitBagAxis = "1a0ff7"
+chitBagAllies = "f5fe52"
+
+diceBagAxis = "45bb1d"
+diceBagAllies = "855803"
+
+controlMarkerBags = {
+    Germany = "4c50aa",
+    Russia = "eb8ab7",
+    Japan = "36d8f4",
+    America = "1d0c20",
+    China = "0ad37d",
+    UKEurope = "34c46a",
+    UKPacific = "d30991",
+    Italy = "40795c",
+    ANZAC = "957100",
+    France = "aa2976",
+}
+
+infantryBags = {
+    Germany = "349428",
+    Russia = "4fc1da",
+    Japan = "62c152",
+    America = "69507b",
+    China = "cc0d76",
+    UKEurope = "6b8e3e",
+    UKPacific = "c358fe",
+    Italy = "bb6130",
+    ANZAC = "54c4a9",
+    France = "bd5542",
+}
+
+artilleryBags = {
+    Germany = "842439",
+    Russia = "462803",
+    Japan = "64d7cd",
+    America = "579060",
+    China = "9ae340",
+    UKEurope = "75d4c9",
+    UKPacific = "529a8e",
+    Italy = "eb0f94",
+    ANZAC = "019bae",
+    France = "a7ac87",
+}
+
+mechInfantryBags = {
+    Germany = "04faf5",
+    Russia = "f91628",
+    Japan = "19a6e3",
+    America = "f67f28",
+    China = nil,
+    UKEurope = "fe8d81",
+    UKPacific = "9db942",
+    Italy = "51b194",
+    ANZAC = "a7e4cc",
+    France = "39dcb5",
+}
+
+tankBags = {
+    Germany = "ffd943",
+    Russia = "28504e",
+    Japan = "6f8dd0",
+    America = "648afb",
+    China = nil,
+    UKEurope = "7b855f",
+    UKPacific = "122994",
+    Italy = "df0240",
+    ANZAC = "1302b1",
+    France = "9590ce",
+}
+
+fighterBags = {
+    Germany = "5e2851",
+    Russia = "43ac06",
+    Japan = "567b4f",
+    America = "6b2477",
+    China = "a4c464",
+    UKEurope = "80329e",
+    UKPacific = "66c5bf",
+    Italy = "64f5d0",
+    ANZAC = "34cc71",
+    France = "661574",
+}
+
+tacBomberBags = {
+    Germany = "a2fc5e",
+    Russia = "bc9568",
+    Japan = "a8430b",
+    America = "0023af",
+    China = nil,
+    UKEurope = "0bc43f",
+    UKPacific = "a9c7a5",
+    Italy = "0fc895",
+    ANZAC = "99d278",
+    France = "873b86",
+}
+
+------ INCOME TRACKING GUIDS
+incomeCounters = {
+    Germany = "3ba250",
+    Russia = "a1d66b",
+    Japan = "7e5daa",
+    America = "62ebe8",
+    China = "b71561",
+    UKEurope = "d8621d",
+    UKPacific = "45eed5",
+    Italy = "b25fab",
+    ANZAC = "526dff",
+    France = "95ef6e",
+}
+
+ipcCounters = {
+    Germany = "7c0498",
+    Russia = "c29511",
+    Japan = "03c2a5",
+    America = "d484dc",
+    China = "3bfaa1",
+    UKEurope = "a3deb5",
+    UKPacific = "e983c7",
+    Italy = "d6ab0c",
+    ANZAC = "5a38c1",
+    France = "40d53c",
+}
+
+collectIncomeButtons = {
+  Germany = "fcbe97",
+  Russia = "412cd8",
+  Japan = "52e8b7",
+  America = "f98df1",
+  China = "8dc26b",
+  UKEurope = "83a872",
+  UKPacific = "8930b4",
+  Italy = "29c3f4",
+  ANZAC = "400b83",
+  France = "767c84",
+}
+
+------ National Objective GUIDS
+nationalObjectiveZones = {
+    Germany = {
+        {"afd1c8", 5, false},
+        {"86b04d", 5, false},
+        {"fb1a0d", 5, false},
+        {"73656d", 5, false},
+        {"d51007", 5, false},
+        {"d51007", 2, false},
+        {"241d74", 2, false},
+        {"241d74", 2, false},
+        {"241d74", 5, false},
+        {"ef2d73", 5, false},
+    },
+    Italy = {
+        {"aed939", 5, false},
+        {"9974c5", 5, false},
+        {"c2f4a7", 5, false},
+        {"5ef8bc", 2, false},
+        {"a64c0e", 2, false},
+        {"0f321a", 2, false},
+    },
+    Japan = {
+        {"e6e534", 5, false},
+        {"c577bb", 5, false},
+        {"950407", 5, false},
+        {"75ab2b", 5, false},
+        {"c27396", 5, false},
+        {"4661a1", 5, false},
+        {"4ebb33", 10, false},
+    },
+    Russia = {
+        {"4339ab", 5, false},
+        {"bc479c", 3, false},
+        {"447276", 10, true},
+    },
+    UKEurope = {
+        {"243162", 5, false},
+    },
+    UKPacific = {
+        {"9afda0", 5, false},
+    },
+    America = {
+        {"20a536", 10, false},
+        {"5390a3", 5, false},
+        {"7b2fb6", 5, false},
+        {"ab3fbd", 5, false},
+        {"5ec66f", 5, false},
+    },
+    China = {
+        {"4d0abb", 6, false},
+    },
+    ANZAC = {
+        {"5532d0", 5, false},
+        {"235154", 5, false},
+    },
+}
+
+------ BATTLE GUIDS
+battleZones = {
+    {"462970", 1},
+    {"16cfc1", 2},
+    {"ee82b8", 3},
+    {"6b504c", 4},
+}
+
+hitCounterAxis = "2bb9b7"
+hitCounterAllies = "0b8c9e"
+
+------ POWERS
+powers = {
+    "Germany",
+    "Russia",
+    "Japan",
+    "America",
+    "China",
+    "UKEurope",
+    "UKPacific",
+    "Italy",
+    "ANZAC",
+    "France"
+}
+
+colorToPower = {
+    White = "Germany",
+    Red = "Russia",
+    Orange = "Japan",
+    Green = "America",
+    Purple = "China",
+    Yellow = "UKEurope",
+    Brown = "Italy",
+    Teal = "ANZAC",
+    Blue = "France",
+}
+
+colorToTeam = {
+  White = "Axis",
+  Red = "Allies",
+  Orange = "Axis",
+  Green = "Allies",
+  Purple = "Allies",
+  Yellow = "Allies",
+  Brown = "Axis",
+  Teal = "Allies",
+  Blue = "Allies",
+}
+
+powerToTeam = {
+    Germany = "Axis",
+    Russia = "Allies",
+    Japan = "Axis",
+    America = "Allies",
+    China = "Allies",
+    UKEurope = "Allies",
+    UKPacific = "Allies",
+    Italy = "Axis",
+    ANZAC = "Allies",
+    France = "Allies",
+}
+
+powerToString = {
+    Germany = "Germany",
+    Russia = "Russia",
+    Japan = "Japan",
+    America = "America",
+    China = "China",
+    UKEurope = "UK (europe)",
+    UKPacific = "UK (pacific)",
+    Italy = "Italy",
+    ANZAC = "ANZAC",
+    France = "France"
+}
+
+------ OBJECT SPAWNING
+local function placeMiniatureFromBag(bag, position, rotation)
+    if not bag then
+        return
+    end
+
+    local rotation1 = rotation or {0, 0, 0}
+
+    local raisedPosition = {position[1], position[2] + 2, position[3]}
+
+    getObjectFromGUID(bag).takeObject({
+            position = raisedPosition,
+            smooth = false,
+            rotation = rotation1
+    })
+end
+
+------ INCOME TRACKING
+local function isFaceUp(obj)
+    local rotation = obj.getRotation()
+    local z = rotation[3]
+
+    return (z > -15 and z < 15)
+end
+
+local function findFaceUpTracker(objs)
+    for _, obj in ipairs(objs) do
+        local name = obj.getName()
+
+        if (string.match(name, "Tracker") or string.match(name, "Occupation")) and isFaceUp(obj) then
+            return obj
+        end
+    end
+
+    return nil
+end
+
+function broadcastString(data)
+    broadcastToAll(data.message, data.params or nil)
+end
+
+local function collectIncome(power)
+    local incomeCounter = getObjectFromGUID(incomeCounters[power]).Counter
+    local ipcCounter = getObjectFromGUID(ipcCounters[power]).Counter
+
+    local baseIncome = incomeCounter.getValue()
+    local bonusIncome = 0
+    local oneTimeIncome = 0
+
+    for _, zoneData in ipairs(nationalObjectiveZones[power]) do
+        local zoneObjects = getObjectFromGUID(zoneData[1]).getObjects()
+        local amount = zoneData[2]
+        local oneTime = zoneData[3]
+
+        local faceUp = findFaceUpTracker(zoneObjects)
+
+        if faceUp then
+            if oneTime then
+                oneTimeIncome = oneTimeIncome + amount
+                faceUp.flip()
+            else
+                bonusIncome = bonusIncome + amount
+            end
+        end
+    end
+
+    ipcCounter.setValue(ipcCounter.getValue() + baseIncome + bonusIncome + oneTimeIncome)
+
+    broadcastToAll(powerToString[power] .. " collects " .. baseIncome + bonusIncome .. " IPCs. " .. baseIncome .. " from Territories, " .. bonusIncome .. " from National Objectives.")
+
+    if oneTimeIncome > 0 then
+        local message = powerToString[power] .. " collects an additional " .. oneTimeIncome .. " IPCs for a one-time bonus!"
+
+        Timer.create({
+            identifier = tostring(math.random()),
+            function_name = "broadcastString",
+            parameters = {message = message},
+            delay = 2
+        })
+    end
+end
+
+function collectIncomeBtn(obj, string_player_color, alt_click)
+    if alt_click then
+        return
+    end
+
+    for _, power in ipairs(powers) do
+        if obj.guid == collectIncomeButtons[power] then
+            collectIncome(power)
+            return
+        end
+    end
+
+    print("Scripting error: power not found")
+end
+
+------ BATTLE STRIP
+function updateBattleTotals()
+    local axisTotal = 0
+    local alliesTotal = 0
+
+    -- Ensure that we only count each die once, in case a die is sitting on the borders of multiple zones
+    local countedDice = {}
+
+    for i, zoneData in pairs(battleZones) do
+        local zone = getObjectFromGUID(zoneData[1])
+        local threshold = zoneData[2]
+
+        for i, die in ipairs(zone.getObjects()) do
+            if countedDice[die.getGUID()] == nil then
+                countedDice[die.getGUID()] = true
+
+                if die.getName() == "Axis Die" and die.getRotationValue() <= threshold then
+                    axisTotal = axisTotal + 1
+                elseif die.getName() == "Ally Die" and die.getRotationValue() <= threshold then
+                    alliesTotal = alliesTotal + 1
+                end
+            end
+        end
+    end
+
+    getObjectFromGUID(hitCounterAxis).Counter.setValue(axisTotal)
+    getObjectFromGUID(hitCounterAllies).Counter.setValue(alliesTotal)
+end
+
+------ DELETE FUNCTION
+local function deleteSelected(player)
+    local objs = {}
+
+    for _,o in ipairs(player.getSelectedObjects()) do objs[#objs+1] = o end
+    for _,o in ipairs(player.getHoldingObjects()) do objs[#objs+1] = o end
+
+    if player.getHoverObject() then
+        objs[#objs+1] = player.getHoverObject()
+    end
+
+    local trashBag = getObjectFromGUID(trashBag)
+
+    for _,o in ipairs(objs) do
+        if not o.getLock() then
+            trashBag.putObject(o)
+        end
+    end
+end
+
+------ TTS CALLBACKS
+function onLoad()
+    for power, buttonGUID in pairs(collectIncomeButtons) do
+        local buttonObj = getObjectFromGUID(buttonGUID)
+
+        buttonObj.createButton({
+            click_function = "collectIncomeBtn",
+            scale = {15, 15, 15}
+        })
+    end
+
+    Timer.create({
+        identifier = "Battle Board Timer",
+        function_name = "updateBattleTotals",
+        delay = 0.1,
+        repetitions = 0,
+    })
+end
+
+function onScriptingButtonDown(index, playerColor)
+    local position = Player[playerColor].getPointerPosition()
+    local power = colorToPower[playerColor]
+    local team = colorToTeam[playerColor]
+
+    if index == 10 then -- Numpad 0: delete
+        deleteSelected(Player[playerColor])
+    elseif index == 1 then -- Numpad 1: Dice
+        if team == "Axis" then
+            placeMiniatureFromBag(diceBagAxis, position, {0, 0, 180})
+        elseif team == "Allies" then
+            placeMiniatureFromBag(diceBagAllies, position, {0, 0, 180})
+        end
+    elseif index == 2 then -- Numpad 2: Control Marker
+        placeMiniatureFromBag(controlMarkerBags[power], position)
+    elseif index == 3 then -- Numpad 3: Chit
+        if team == "Axis" then
+            placeMiniatureFromBag(chitBagAxis, position)
+        elseif team == "Allies" then
+            placeMiniatureFromBag(chitBagAllies, position)
+        end
+    elseif index == 4 then -- Numpad 4: Infantry
+        placeMiniatureFromBag(infantryBags[power], position)
+    elseif index == 5 then -- Numpad 5: Artillery
+        placeMiniatureFromBag(artilleryBags[power], position)
+    elseif index == 6 then -- Numpad 6: Mechanized Infantry
+        placeMiniatureFromBag(mechInfantryBags[power], position)
+    elseif index == 7 then -- Numpad 7: Tank
+        placeMiniatureFromBag(tankBags[power], position)
+    elseif index == 8 then -- Numpad 8: Fighter
+        placeMiniatureFromBag(fighterBags[power], position)
+    elseif index == 9 then -- Numpad 9: Tactical Bomber
+        placeMiniatureFromBag(tacBomberBags[power], position)
+    end
+end
